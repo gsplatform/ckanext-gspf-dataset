b547c72fc831ef4f8d4981f15e147671d03e62bc
ckaner <ckaner@localhost.localdomain>
2015-02-05 00:54:40 +0000

    initial

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+    name='ckanext-gspf-dataset',
+    version=version,
+    description="Geospatial Platform dataset plugin",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Hal Seki',
+    author_email='hal@georepublic.co.jp',
+    url='http://georepublic.co.jp/',
+    license='AGPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.gspf-dataset'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        # Add plugins here, e.g.
+        # myplugin=ckanext.gspf-dataset.plugin:PluginClass
+    ''',
+)
